793d795b4eb995dbcda4a1b4422211d134e8eb88
Alexis Milon <amilon@hawk.iit.edu>
2018-02-09 18:28:56 -0600

    Add variables for jQuery to validate (name, email, and phone number)

---
+++ js/site.js
@@ -0,0 +1,15 @@
+$('#contact-form').on('submit',
+  function(e)) {
+
+    var name = $('#name').val();
+    var valid_name = true;
+
+    var email = $('#email').val();
+    var valid_email = /.+@.+/;
+
+    var phone = $('#phone').val();
+    var valid_phone = /\d{3}.+\d{3}.+\d{4}/;
+
+
+
+  };
