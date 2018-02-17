ac35b4a1ba695b516b2921a28b7c55b3f7ee0e1d
Alexis Milon <amilon@hawk.iit.edu>
2018-02-16 20:17:55 -0600

    Add email function to test user data

--- js/site.js
+++ js/site.js
@@ -4,37 +4,19 @@ $('.user_form li').on('click', function() {
   $(this).find('user_form').focus();
 });
 
-    var email = $('#email').val();
-    var valid_email = /.+@.+/;
-
-    var phone = $('#phone').val();
-    var valid_phone = /\d{3}.+\d{3}.+\d{4}/;
-
-    if(name == '' || name == null) {
-      console.log('invalid name');
-      $(this).remove();
-      $('label [for="name"]').append('<p class="invalid"> *Ops, not quite. Please enter a first AND last name*</p>');
-      return false;
-    }
-
-    else if(!valid_email.test(email)) {
-      console.log('invalid email');
-      $(this).remove();
-      $('label [for="email"]').append('<p class="invalid"> *Ops, not quite. Please enter a valid email address (you@example.com)*</p>');
-      return false;
-    }
-
-    else if(!valid_phone.test(phone)) {
-      console.log('invalid phone');
-      $(this).remove();
-      $('label [for="phone"]').append('<p class="invalid"> *Ops, not quite. Please enter a phone number in the following format (###-###-####)*</p>');
-      return false;
-    }
-
-    else {
-      console.log($('#name').val(), $('#email').val(), $('#phone').val());
-      $(this).remove();
-      $('#content').append('<p class="valid"> Thank you ' + name +' , you have been added to our mailing list! </p>');
-    }
+$('#email').on('keyup', function() {
+  var email = {
+    val: $(this).val(),
+    pat: /.+@.+/
+  }
+  if((email.pat).test(email.val)) {
+    console.log('working');
+  }
+  else {
+    console.log('invalid email');
+    $(this).remove();
+    $('label [for="email"]').append('<p class="invalid"> *Ops, not quite. Please enter a valid email address (you@example.com)*</p>');
+  }
+});
 
   };
