6108b519e86cf99c67ed2e94f08cddf546aa2277
Alexis Milon <amilon@hawk.iit.edu>
2018-02-18 21:43:02 -0600

    Add validation test for email field

--- js/site.js
+++ js/site.js
@@ -14,33 +14,16 @@
     e.preventDefault();
   });
 
-  if(!pattern.email.test(inputs.email)) {
-    console.log('Invalid Email');
-    $('.invalid').remove();
-    $('.input-email').append('<p class="invalid"> *A valid email address; (e.g., you@example.com)*</p>');
-  }
-  else {
-    console.log('Valid Email');
-    validator.email = true;
-  }
+  function valid_email(email) {
+    var result = false;
 
-  if(!pattern.phone.test(inputs.phone)) {
-    console.log('Invalid Phone');
-    $('.invalid').remove();
-    $('.input-phone').append('<p class="invalid"> *A valid phone number; (###-###-####)*</p>');
-  }
-  else {
-    console.log('Valid Phone');
-    validator.phone = true;
-  }
+    if (email.length > 0) {
+      if ((/^[^\s@]+@[^\s@]+$/).test(email)) {
+        result = true;
+      }
+    }
 
-  if(validator.email === true && validator.phone === 'undefined') {
-    console.log('Only email!');
-    $(this).remove();
-    $('#project').remove();
-    $('#project').append("<h1>Let's save the net!</h1>");
-    $('.user-form').remove('#email');
-    $('#email').append("<p>Thank You for taking the time to join our mailing list! We will be keeping in touch about everything 'Net Neutrality'.</p");
+    return result;
   }
 
   if(validator.phone === true && validator.email === 'undefined') {
